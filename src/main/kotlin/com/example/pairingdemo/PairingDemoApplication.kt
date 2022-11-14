ecbf252bc7c6fe7e7872053ec6a9a7c4cab2aaba
Wen Ting Wang <wenting@madetech.com>
2022-11-14 15:43:04 +0000

    Refactor: Get external book API host from config

--- src/main/kotlin/com/example/pairingdemo/PairingDemoApplication.kt
+++ src/main/kotlin/com/example/pairingdemo/PairingDemoApplication.kt
@@ -1,9 +1,11 @@
 package com.example.pairingdemo
 
+import com.example.pairingdemo.config.ExternalBooksApiProperties
 import com.example.pairingdemo.models.Book
 import com.example.pairingdemo.models.BookDTO
 import org.apache.tomcat.util.json.JSONParser
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,9 +16,9 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
-import kotlin.reflect.typeOf
 
 @SpringBootApplication
+@EnableConfigurationProperties(ExternalBooksApiProperties::class)
 class PairingDemoApplication
 
 fun main(args: Array<String>) {
@@ -24,7 +26,7 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class ApiController {
+class ApiController(val externalBooksApi: ExternalBooksApiProperties) {
     var books = mutableListOf(
         Book(id = "1", title = "Clean Architecture", author = "Robert Martin"),
         Book(id = "2", title = "Pragmatic Programmer", author = "David Thomas")
@@ -40,16 +42,21 @@ class ApiController {
     fun createBook(@RequestBody book: Book) = books.add(book)
 
     @GetMapping("/externalBook")
-    fun something(): Book {
+    fun externalBook(): Book {
         val client = HttpClient.newBuilder().build()
         val request = HttpRequest.newBuilder()
-            .uri(URI.create("https://simple-books-api.glitch.me/books/1"))
+            .uri(URI.create("${externalBooksApi.host}/books/1"))
             .build()
 
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
         val externalBook = JSONParser(response.body()).parseObject();
 
-        var bookDTO = BookDTO(externalBook["id"].toString(), externalBook["name"].toString(), externalBook["author"].toString(), externalBook["available"] as Boolean)
+        var bookDTO = BookDTO(
+            externalBook["id"].toString(),
+            externalBook["name"].toString(),
+            externalBook["author"].toString(),
+            externalBook["available"] as Boolean
+        )
 
         return bookDTO.toBook();
     }
