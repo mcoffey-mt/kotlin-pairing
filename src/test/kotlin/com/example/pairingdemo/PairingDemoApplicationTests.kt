8f180623d8718653460edee52ddb78f8c30bfcfd
Wen Ting Wang <wenting@madetech.com>
2022-11-25 15:38:45 +0000

    Update tests for GET external book to be integration tests
    - Move into PairingDemoApplicationTests and use SpringBooTest instead.

--- src/test/kotlin/com/example/pairingdemo/PairingDemoApplicationTests.kt
+++ src/test/kotlin/com/example/pairingdemo/PairingDemoApplicationTests.kt
@@ -1,13 +1,71 @@
 package com.example.pairingdemo
 
-import org.junit.jupiter.api.Test
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.wiremock.ListenerMode
+import io.kotest.extensions.wiremock.WireMockListener
+import io.kotest.matchers.shouldBe
+import org.json.JSONObject
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 
 @SpringBootTest
-class PairingDemoApplicationTests {
+@TestConfiguration
+@AutoConfigureMockMvc
+class PairingDemoApplicationTests(@Autowired val mockMvc: MockMvc) : DescribeSpec({
+    describe("GET /externalBooks/{id}") {
+        val externalBooksApi = WireMockServer(9000)
+        val id = 1
+        val book = mapOf("id" to id, "name" to "The Batman", "author" to "Bruce Wayne", "available" to true)
 
-    @Test
-    fun contextLoads() {
+        beforeTest {
+            listener(WireMockListener(externalBooksApi, ListenerMode.PER_SPEC))
+            externalBooksApi.start()
+        }
+
+        afterTest { externalBooksApi.stop() }
+
+        describe("when an external book is successfully retrieved") {
+            beforeTest {
+                externalBooksApi.stubFor(
+                    WireMock.get(WireMock.urlEqualTo("/books/$id"))
+                        .willReturn(WireMock.okJson(JSONObject(book).toString()))
+                )
+            }
+
+            it("responds with 200 status") {
+                val result = mockMvc.perform(MockMvcRequestBuilders.get("/externalBooks/$id")).andReturn()
+
+                result.response.status.shouldBe(200)
+            }
+
+            it("returns a book with the matching ID") {
+                val result = mockMvc.perform(MockMvcRequestBuilders.get("/externalBooks/$id")).andReturn()
+
+                JSONObject(result.response.contentAsString)["id"].shouldBe("$id")
+                JSONObject(result.response.contentAsString)["title"].shouldBe("The Batman")
+                JSONObject(result.response.contentAsString)["author"].shouldBe("Bruce Wayne")
+            }
+        }
+
+        describe("when an external book is not found") {
+            beforeTest {
+                externalBooksApi.stubFor(
+                    WireMock.get(WireMock.urlEqualTo("/books/$id"))
+                        .willReturn(WireMock.notFound())
+                )
+            }
+
+            it("responds with 404 status") {
+                val result = mockMvc.perform(MockMvcRequestBuilders.get("/externalBooks/$id")).andReturn()
+
+                result.response.status.shouldBe(404)
+            }
+        }
     }
-
-}
+})
